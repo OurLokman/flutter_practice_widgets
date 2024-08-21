7370075032831958f27b2fe0e7f48343fedccbad
Dart Basic to Advanced <mhlokman10@gmail.com>
2024-08-21 10:46:24 +0600

    layoutbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo Home Page'),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth > 600) {
+              return Column(
+                children: [
+                  Text(
+                    'Responsive Layout',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    height: 150,
+                    width: 150,
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        'Large Screen Layout',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Image.network(
+                    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308',
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              );
+            } else {
+              return Column(
+                children: [
+                  Text(
+                    'Responsive Layout',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                    child: Center(
+                      child: Text(
+                        'Small Screen Layout',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Image.network(
+                    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308',
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              );
+            }
+          },
         ),
-        body: Container(
-          color: Colors.red,
-          width: double.infinity,
-          height: 100,
-          alignment: Alignment.center,
-          child: AspectRatio(
-            aspectRatio: 16/9,
-            child: Container(
-              color: Colors.green,
-                  child: Center(
-                child: Text('16:9',
-                  style: TextStyle(color: Colors.white, fontSize: 20),),
-            ),
-            ),
-          ),
-        )
       ),
     );
   }
